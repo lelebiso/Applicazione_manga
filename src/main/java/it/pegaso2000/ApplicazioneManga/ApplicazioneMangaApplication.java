7d32d71a61073947ee660d5ce0243d7dc597917d
Emanuele Biselli <emanuele.biselli@pegaso2000.it>
2023-06-26 09:43:27 +0200

    progetto vuoto
    primo commit

---
+++ src/main/java/it/pegaso2000/ApplicazioneManga/ApplicazioneMangaApplication.java
@@ -0,0 +1,13 @@
+package it.pegaso2000.ApplicazioneManga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicazioneMangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicazioneMangaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/pegaso2000/ApplicazioneManga/ApplicazioneMangaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pegaso2000.ApplicazioneManga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicazioneMangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
